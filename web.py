bea607ebd82517b9d93a7d3987728243d95428db
aswathipv <64973786+aswathipv@users.noreply.github.com>
2022-07-24 22:38:38 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+   co= request.values['county']
+   st= request.values['state']
+   cy= request.values['city']
+   mapy={1:'poor',2:'Moderate',3:'Good'}
+   query=np.array([co,st,cy])
+   query=query.astype(np.float64)
+   query=query.reshape(1,-1)
+   out=mapy[model.predict(query)[0]]
+   return render_template ('result.html',prediction_text="The Quality of Air is.{}" ,name=out)
+   #output=model.predict(exp1)
+   #return render_template ('result.html',prediction_text="The Quality of Air is.{}")
+if __name__=='__main__':
+    app.run()
+
