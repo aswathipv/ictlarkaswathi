d3965a677fecd0abb8c158b73179bb1db94b8ff4
aswathipv <64973786+aswathipv@users.noreply.github.com>
2022-07-27 15:51:45 +0530

    Add files via upload

--- web.py
+++ web.py
@@ -11,14 +11,15 @@ def predict():
    co= request.values['county']
    st= request.values['state']
    cy= request.values['city']
+   yr= request.values['year']
    mapy={1:'poor',2:'Moderate',3:'Good'}
-   query=np.array([co,st,cy])
+   query=np.array([co,st,cy,yr])
    query=query.astype(np.float64)
    query=query.reshape(1,-1)
    out=mapy[model.predict(query)[0]]
-   return render_template ('result.html',prediction_text="The Quality of Air is.{}" ,name=out)
+   return render_template ('result.html',prediction_text="The Quality of Air is." ,name=out)
    #output=model.predict(exp1)
-   #return render_template ('result.html',prediction_text="The Quality of Air is.{}")
+   #return render_template ('result.html',)
 if __name__=='__main__':
     app.run()
 
